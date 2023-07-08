f9fbff25ec6fb4fd20f1d45a40d4c3eb751e0639
Monel.D <monelyehoun@gmail.com>
2023-07-08 10:59:26 +0200

    ...

--- src/app/pages/backend/home.tsx
+++ src/app/pages/backend/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Accordion, Carousel } from 'react-bootstrap';
 import { ListGames } from '../../service/frontendService';
 
 const Home = () => {
@@ -20,9 +20,10 @@ const Home = () => {
                         <Carousel.Item key={index}>
                             <img
                                 className="d-block w-100"
-                                style={{ height: '500px' }}
+                                style={{ height: '500px',aspectRatio: 16 / 9  }}
                                 src={el.miniature}
                                 alt={el.name}
+                               
                             />
                             <Carousel.Caption>
                                 <h3>{el.name}</h3>
@@ -36,6 +37,35 @@ const Home = () => {
                     </h2>
                 )}
             </Carousel>
+
+            <div className="container-fluid mt-5 pt-2">
+            <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+            </div>
         </div>
     );
 };
